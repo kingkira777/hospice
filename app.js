4a972d01488d68c048785496fc3a3f3319b4604a
EvilGod <evilgod777@protonmail.com>
2020-05-03 04:30:14 +0800

    redirect added

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.get('*',(req,res,next)=>{
+  console.log(req.headers.host);
+  next();
+})
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
