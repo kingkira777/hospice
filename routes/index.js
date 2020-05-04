0003f93cb9d2d06ac324a1bf8f460214ccb6fe88
EvilGod <evilgod777@protonmail.com>
2020-05-05 04:13:19 +0800

    x

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var ishttp = req.protocol;
-  if(ishttp === "http"){
-    res.redirect('https://dev.triunionhealthcare.com/');
+  console.log(ishttp);
+  // if(ishttp === "http"){
+  //   res.redirect('https://dev.triunionhealthcare.com/');
+  //   res.end();
+  // }else{
+    
+  // }
+  res.render('index', { title: 'Express' });
     res.end();
-  }else{
-    res.render('index', { title: 'Express' });
-    res.end();
-  }
 });
 
 module.exports = router;
